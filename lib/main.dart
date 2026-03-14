ef5533f216ef776eacf5bdf9ecf6cdad43d9be94
Maulanahb <maulanahmad2287@gmail.com>
2026-03-14 22:10:35 +0700

    fix problem

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,16 @@
 import 'package:flutter/material.dart';
+import 'pages/home_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Sport Schedule',
       home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Sport Schedule App"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              child: Text("Lihat Jadwal"),
-              onPressed: () {},
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              child: Text("Tambah Jadwal"),
-              onPressed: () {},
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
